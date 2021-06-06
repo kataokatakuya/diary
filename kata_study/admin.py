b7c6720297c51b4a42c1cfb40ae147defdf30780
kataokataukuya <kataokat.meijo@gmail.com>
2021-06-06 23:40:12 +0900

    edit models.py
    models.py を編集しました。

--- kata_study/admin.py
+++ kata_study/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Record
+from .models import Topic
 
-# Register your models here.
+admin.site.register(Record)
+admin.site.register(Topic)
--- kata_study/models.py
+++ kata_study/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Recordクラス
+class Record(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan_text = models.TextField(max_length=1000)
+    do_text = models.TextField(max_length=1000, blank=True, null=True)
+    check_text = models.TextField(max_length=1000, blank=True, null=True)
+    action_text = models.TextField(max_length=1000, blank=True, null=True)
+    plan_date = models.DateField(blank=True, null=True)
+    do_start = models.DateField(blank=True, null=True)
+    do_end = models.DateField(blank=True, null=True)
+    check_date = models.DateField(blank=True, null=True)
+    action_date = models.DateField(blank=True, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+        
+    class Meta:
+        ordering = ('-plan_date',)
+
+# Topicクラス
+class Topic(models.Model):
+    title = models.TextField(max_length=100)
+    text = models.TextField(max_length=300)
+    image = models.TextField(max_length=30)
+    genre = models.TextField(max_length=30)
+    url = models.TextField(max_length=30)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('pub_date',)
