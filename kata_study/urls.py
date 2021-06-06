a6b8f08c74cc3db8c19b094301f8f446237bea79
kataokataukuya <kataokat.meijo@gmail.com>
2021-06-07 00:56:26 +0900

    add mch_list.html
    mch_list.htmlを追加しました。

--- kata_study/urls.py
+++ kata_study/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('mch', views.mch, name='mch'),
+    path('mch/<int:num>', views.mch, name='mch'),
 ]
--- kata_study/views.py
+++ kata_study/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import Record, Topic
+
 
 def index(request):
     return render(request, 'kata_study/index.html')
+
+# 機械学習リストページの表示
+def mch(request, num=1):
+    data = Topic.objects.filter()
+    page = Paginator(data, 4)
+    params = {
+        'topics': page.get_page(num),
+    }
+    return render(request, 'kata_study/mch_list.html', params)
